e564f5a52a8191e4b2b5bf07f2ec77734f066c08
akshay <akshay.mishra@5exceptions.com>
2022-03-23 13:20:23 +0530

    final commit

--- ios/Classes/FlutterForegroundTaskPlugin.m
+++ ios/Classes/FlutterForegroundTaskPlugin.m
@@ -5,7 +5,7 @@
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_foreground_task-Swift.h"
+#import "flutter_foreground_task_native-Swift.h"
 #endif
 
 @implementation FlutterForegroundTaskPlugin
