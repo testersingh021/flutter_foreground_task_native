22c01ce0bf5f5e3c0173fe5ec0b73a2d6024650e
Akshay <50194167+rajeev5x@users.noreply.github.com>
2022-07-05 19:30:21 +0530

    ci-cd 26th attempt

--- pub_login.sh
+++ pub_login.sh
@@ -25,10 +25,10 @@ fi
 
 # Create credentials.json file.
   echo "Copy credentials"
-  mkdir -p /opt/hostedtoolcache/flutter/1.9.1-hotfix.2-stable/x64/.pub-cache/
+  #mkdir -p /opt/hostedtoolcache/flutter/1.9.1-hotfix.2-stable/x64/.pub-cache/
 #  echo "$CREDENTIAL_JSON" > ~/.pub-cache/credentials.json
 #  CREDENTIAL_JSON="secrets.CREDENTIAL_JSON"
-  cat <<EOF > /opt/hostedtoolcache/flutter/1.9.1-hotfix.2-stable/x64/.pub-cache/credentials.json
+  cat <<EOF > HOME/.config/dart/pub-credentials.json
 {
   "accessToken":"${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
   "refreshToken":"${PUB_DEV_PUBLISH_REFRESH_TOKEN}",
